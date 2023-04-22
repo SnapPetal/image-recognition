9ff9ee764e1d6ea0612ee0c9cc741a0f938bb25a
Thon Becker <thon.becker@gmail.com>
2023-04-22 11:56:05 +0000

    Initial commit

---
+++ test/image-recognition.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ImageRecognition = require('../lib/image-recognition-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-recognition-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ImageRecognition.ImageRecognitionStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
